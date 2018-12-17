dec6f5ecc3a2cd32414b0b80d2f0aac8b73c38a1
lantost <lantos.tibor@kifu.gov.hu>
2018-12-17 16:31:02 +0100

    FEAT: add card component and bulma

--- src/components/productsContent.js
+++ src/components/productsContent.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
 import '../styles/styles.scss';
+
+import { Card } from '../components/card';
 
 export class ProductsContent extends React.Component {
 
   render() {
     return (
       <div id='products-content'>
+        <Card></Card>
       </div>
     )
   }
